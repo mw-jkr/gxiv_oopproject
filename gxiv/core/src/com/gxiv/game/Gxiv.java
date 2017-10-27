4e21eeecc657efa8f7a5ca89d0bc386968ae41c9
liaria <59070141@it.kmitl.ac.th>
2017-10-27 18:51:38 +0700

    Add sample main menu

---
+++ gxiv/core/src/com/gxiv/game/Gxiv.java
@@ -0,0 +1,27 @@
+package com.gxiv.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Gxiv extends Game {
+    public SpriteBatch batch;
+    public BitmapFont font;
+
+    public void create() {
+        batch = new SpriteBatch();
+        //Use LibGDX's default Arial font.
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+    }
+
+    public void render() {
+        super.render(); //important!
+    }
+
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+
+}
