0e3c84f731e7568ab02c8560874fac78e658c483
liaria <59070141@it.kmitl.ac.th>
2017-11-06 14:06:59 +0700

    fix gxiv conflict

--- core/src/com/gxiv/game/gxiv.java
+++ core/src/com/gxiv/game/gxiv.java
@@ -1,27 +1,29 @@
 package com.gxiv.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.gxiv.game.screen.MainMenuScreen;
+import com.gxiv.game.util.AssetsManager;
 
-public class gxiv extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class Gxiv extends Game {
+    public SpriteBatch batch;
+    public BitmapFont font;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        AssetsManager.load();
+        this.setScreen(new MainMenuScreen(this));
+    }
+
+    public void render() {
+        super.render();
+    }
+
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+
 }
